45e91ba5e448dadeef6ca40acb16c903e78d5e76
Kyl23 <cklkailiang@gmail.com>
2022-04-21 19:11:27 +0800

    feat: CSVReader and template

---
+++ CSVReader.hpp
@@ -0,0 +1,121 @@
+#ifndef Gene_Algo_Lib_IO_Reader_CSV
+#define Gene_Algo_Lib_IO_Reader_CSV
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream> //stream
+
+using namespace std;
+
+class CSVReader{
+private:
+    string pathOfFile;
+    char delim = 0;
+    string temp,nextLine;
+    string buffer;
+
+    ifstream file ;
+    vector<string> returnValue;
+    vector<vector<string>> returnTable;
+    void initBuffer(){
+        buffer = "";
+    }
+    void initVectorAndBuffer(){
+        initBuffer();
+        returnValue.clear();
+    }
+    //for t_readLine, help for debug
+    bool t_hasNextLine = true;
+    string t_temp,t_nextLine;
+    vector<string> t_readLine(ifstream &t_File){
+        vector<string> t_file;
+        string t_buffer;
+        try {
+            if(t_temp.empty()){
+                getline(file, t_temp);
+                getline(file, t_nextLine);
+            }
+            else{
+                t_temp = t_nextLine;
+                getline(file, t_nextLine);
+            }
+            if(file.eof()) {
+                t_hasNextLine = false;
+                t_File.close();
+            }
+            for(auto &i:t_temp){
+                if(i == delim){
+                    t_file.push_back(t_buffer);
+                    t_buffer = "";
+                    continue;
+                }
+                t_buffer += i;
+            }
+            t_file.push_back(t_buffer);//by last value
+        } catch (exception) {
+            std::cout << "temporary readLine error";
+        }
+        return t_file;
+    }
+public:
+    explicit CSVReader(string path,char pattern = ','){
+        pathOfFile = path;
+        delim = pattern;
+        file.open(pathOfFile);
+        if(!file.is_open()) std::cout << "can't_file open file";
+    }
+    bool hasNextLine = true;
+    vector<string> readLine(){
+        try {
+            initVectorAndBuffer();
+            if(temp.empty()){
+                getline(file, temp);
+                getline(file, nextLine);
+            }
+            else{
+                temp = nextLine;
+                getline(file, nextLine);
+            }
+            if(file.eof()) {
+                hasNextLine = false;
+                file.close();
+            }
+            for(auto &i:temp){
+                if(i == delim){
+                    returnValue.push_back(buffer);
+                    initBuffer();
+                    continue;
+                }
+                buffer += i;
+            }
+            returnValue.push_back(buffer);//by last value
+        } catch (exception) {
+            std::cout << "readLine error";
+        }
+        return returnValue;
+    }
+    vector<vector<string>> readTable(){
+        try{
+            returnTable.clear();
+            while(hasNextLine){
+                returnTable.push_back(readLine());
+            }
+        }catch(exception){
+            std::cout << "readTable err";
+        }
+        return returnTable;
+    }
+    void print(){
+        ifstream t_File(pathOfFile);
+        while(t_hasNextLine){
+            for(auto &i : t_readLine(t_File))
+                cout << i << " ";
+            cout << endl;
+        }
+        t_File.close();
+    }
+};
+#endif
+
+
---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include "CSVReader.hpp"
+
+int main(){
+    /*
+     * CSVReader reader({path here},{separate symbol:options,default:','});
+     *
+     * */
+
+
+
+    /*/////////////////////////////////////////////
+     * ReadTable and print both can only use once in an initial
+     * Can't mix readTable and readLine function besides reinitialize
+     * */
+
+    /*  Read a whole table return as a vector<vector<string>>
+     *
+     * reader.readTable()
+     * */
+
+
+    /* Read as line
+     *
+     * reader.readLine()
+     * reader.hasNextLine()
+     * */
+
+
+    /* Immediate print the csv file
+     *
+     * reader.print()
+     * */
+
+    return 0;
+}
